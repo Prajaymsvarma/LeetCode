b4273fc395bdc9915aa9d2fb3522978e7b3577fa
Prajaymsvarma <151991400+Prajaymsvarma@users.noreply.github.com>
2026-06-09 14:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (76.87%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,16 +12,11 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        // Base case: If the node is null, the depth is 0
         if (root == nullptr) {
             return 0;
         }
-        
-        // Recursively find the depth of the left and right subtrees
         int leftDepth = maxDepth(root->left);
         int rightDepth = maxDepth(root->right);
-        
-        // The total depth is 1 (for the current root) + the max depth of its children
         return 1 + std::max(leftDepth, rightDepth);
     }
 };
